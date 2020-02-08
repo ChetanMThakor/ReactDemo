4343da17446861f4b646dfc23e4f7be46875ed1d
Chetan Thakor <60822395+ChetanMThakor@users.noreply.github.com>
2020-02-08 23:10:33 +0530

    Initial commit

---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './Layouts/Header'
+import Footer from './Layouts/Footer'
+export default props => (
+  <div>
+    <Header />
+    <h1>Hello Mr. Shabbir Vaghela ites my first react application</h1>
+    <Footer />
+  </div>
+)
