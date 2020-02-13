6d1a958c3f68c4fa90dc2accb931f7ca6aa39348
Chetan Thakor <60822395+ChetanMThakor@users.noreply.github.com>
2020-02-13 11:02:47 +0530

    change
    change content

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Header from './Layouts/Header'
-import Footer from './Layouts/Footer'
+import React from "react";
+import Header from "./Layouts/Header";
+import Footer from "./Layouts/Footer";
 export default props => (
   <div>
     <Header />
-    <p>Hello Mr. Shabbir Vaghela ites my first react application</p>
+    <p>Hello my first react application</p>
     <Footer />
   </div>
-)
+);
